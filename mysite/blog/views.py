14a6e1b9ea4021b798d09ce1ea220ac017070087
Vadim Andreev <vadimcola@mail.ru>
2023-07-13 18:05:27 +0700

    no comment

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -28,4 +29,10 @@ def post_detail(request, year, month, day, post):
                              )
     return render(request, 'blog/post/detail.html', {'post': post})
 
+
+class PostListView(ListView):
+     queryset = Post.published.all()
+     context_object_name = 'posts'
+     paginate_by = 3
+     template_name = 'blog/post/list.html'
 
