49e07f9c0a58667fcf94a2bd810634de569a828f
Vadim Andreev <vadimcola@mail.ru>
2023-07-16 23:16:05 +0700

    no message

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
+from .forms import EmailPostForm
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
@@ -31,8 +32,20 @@ def post_detail(request, year, month, day, post):
 
 
 class PostListView(ListView):
-     queryset = Post.published.all()
-     context_object_name = 'posts'
-     paginate_by = 3
-     template_name = 'blog/post/list.html'
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
